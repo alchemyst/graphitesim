962217e77542cec8c2c56c9c6d480c4c782d69ef
Carl Sandrock <carl.sandrock@up.ac.za>
2013-01-15 20:32:57 +0200

    Code cleanup

--- flatflakec.m
+++ flatflakec.m
@@ -8,10 +8,9 @@ nsize=504;%1004; THIS IS X
 
 makerandhole=false;%true;
 makerand=false;%true;
-% numblocks=1200;
-% blocksize=30;
-numblocks=1500;
-blocksize=45;
+
+numblocks=1500; %1200
+blocksize=45; %30
 makecirc=false;%true;
 radius=250;
 maketriangle=false;%true;
@@ -23,15 +22,7 @@ makecirchole=false;%true;
 cat=true;
 catreacstep=1;
 catdist=10000;
-% msize=1004;%5001;
-% nsize=1004;%5001;
-% catdist=1000;
-normreacT=0.1;%0.0029;%0.05;%0.586569419;%0.7038833;%0.11111;
-% normreacT=0.003652;%0.01436;%0.05;%0.586569419;%0.7038833;%0.11111;
-% normreac=0.1;
-% normreac=0.25;
-% catreac=0.64;
-% normreac=0.045;
+normreacT=0.1;
 normreact=1;   %for entire edge react sim
 % numcat=50;
 catreac=1;%;normreac*10;
@@ -39,118 +30,16 @@ catreac=1;%;normreac*10;
 % normreac=1;
 % normreact=10;
 
-flakem=ones(msize,nsize);
-% flakem(1:msize,800:nsize)=0;
-flakem(1,:)=0;
-flakem(2,:)=0;
-flakem(:,1)=0;
-flakem(:,2)=0;
-flakem(msize,:)=0;
-flakem(msize-1,:)=0;
-flakem(:,nsize)=0;
-flakem(:,nsize-1)=0;
-
-% flakem(125,125)=0;
-% flakem(125,375)=0;
-% flakem(375,125)=0;
-% flakem(375,375)=0;
-% flakem(167,167)=0;
-% flakem(167,333)=0;
-% flakem(333,167)=0;
-% flakem(333,333)=0;
-% flakem(50,50)=0;
-% flakem(50,450)=0;
-% flakem(450,50)=0;
-% flakem(450,450)=0;
+flakem=zeros(msize,nsize);
+flakem(3:end-2, 3:end-2) = 1;
 flakem(50,50:450)=0;
 flakem(450,50:450)=0;
 flakem(100:400,250)=0;
-% flakem(167,125:375)=0;
-% flakem(333,125:375)=0;
-% flakem(125:375,167)=0;
-
-% flakem=zeros(msize,nsize);
-% flakem(133:266,3:397)=1;
-% flakem(3:397,133:266)=1;
-% flakem=ones(msize,nsize);
 
 if makecross
-	flakem(200:800,499)=0;
-	flakem(200:800,500)=0;
-	flakem(200:800,501)=0;
-	flakem(500,200:800)=0;
-	flakem(499,200:800)=0;
-	flakem(500,200:800)=0;
+	flakem(200:800,499:501)=0;
+	flakem(499:501,200:800)=0;
 end;
-
-% flakem(100:900,249)=0;
-% flakem(100:900,250)=0;
-% flakem(100:900,251)=0;
-% flakem(100:900,749)=0;
-% flakem(100:900,750)=0;
-% flakem(100:900,751)=0;
-% flakem(750:1000,500)=0;
-% flakem(750:1000,499)=0;
-% flakem(750:1000,501)=0;
-% flakem(500,1:250)=0;
-% flakem(499,1:250)=0;
-% flakem(501,1:250)=0;
-% flakem(500,750:1000)=0;
-% flakem(499,750:1000)=0;
-% flakem(501,750:1000)=0;
-% flakem(50:450,250)=0;
-% flakem(250,1:150)=0;
-% flakem(250,350:500)=0;
-% flakem(900:1004,500)=0;
-% flakem(1:998,1)=1;
-% flakem(1:998,2)=1;
-% flakem(1:998,1004)=1;
-% flakem(1:998,1003)=1;
-% flakem(1,:)=1;
-% flakem(2,:)=1;
-%  flakem(301,1:400)=0;
-%  flakem(301:404,100)=0;
-%  flakem(301:404,200)=0;
-%  flakem(301:404,300)=0;
-%  flakem(400-round(rand*100),round(rand*100))=0;
-%  flakem(400-round(rand*100),round(rand*100))=0;
-%  flakem(400-round(rand*100),round(rand*100))=0;
-%  flakem(400-round(rand*100),round(rand*100))=0;
-%  flakem(400-round(rand*100),100+round(rand*100))=0;
-%  flakem(400-round(rand*100),100+round(rand*100))=0;
-%  flakem(400-round(rand*100),100+round(rand*100))=0;
-%  flakem(400-round(rand*100),100+round(rand*100))=0;
-%  flakem(400-round(rand*100),200+round(rand*100))=0;
-%  flakem(400-round(rand*100),200+round(rand*100))=0;
-%  flakem(400-round(rand*100),200+round(rand*100))=0;
-%  flakem(400-round(rand*100),200+round(rand*100))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(round(rand*(msize-4)+2),round(rand*(nsize-4)+2))=0;
-%  flakem(1:200,250)=0;
-%  flakem(300:500,250)=0;
-%  flakem(200,1:100)=0;
-%  flakem(200,250:450)=0;
-%  flakem(200:300,150)=0;
-%  flakem(150,1:100)=0;
-%  flakem(150,200:300)=0;
 
 if makerand
     flakem=zeros(msize,nsize);
@@ -173,19 +62,15 @@ if makerand
             end;
         end;
     end;
-flakem(50:950,50:950)=1;
-flakem(1,:)=0;
-flakem(2,:)=0;
-flakem(:,1)=0;
-flakem(:,2)=0;
-flakem(msize,:)=0;
-flakem(msize-1,:)=0;
-flakem(:,nsize)=0;
-flakem(:,nsize-1)=0;
-% flakem(round(rand*msize),round(rand*nsize))=0;
-% flakem(round(rand*msize),round(rand*nsize))=0;
-% flakem(round(rand*msize),round(rand*nsize))=0;
-% flakem(round(rand*msize),round(rand*nsize))=0;
+    flakem(50:950,50:950)=1;
+    flakem(1,:)=0;
+    flakem(2,:)=0;
+    flakem(:,1)=0;
+    flakem(:,2)=0;
+    flakem(msize,:)=0;
+    flakem(msize-1,:)=0;
+    flakem(:,nsize)=0;
+    flakem(:,nsize-1)=0;
 end;
 
 if makecirc
@@ -211,10 +96,6 @@ if makecirchole
 end;
 
 if makerandhole
-% 	flakem(round(rand*msize*0.25)+100-10,round(rand*nsize*0.25)+200-10)=0;
-% 	flakem(round(rand*msize*0.25)+600-10,round(rand*nsize*0.25)+200-10)=0;
-% 	flakem(round(rand*msize*0.25)+600-10,round(rand*nsize*0.25)+700-10)=0;
-% 	flakem(round(rand*msize*0.25)+100-10,round(rand*nsize*0.25)+700-10)=0;
     for i = 1:8
         flakem(round(rand*msize),round(rand*nsize))=0;
     end
@@ -238,9 +119,9 @@ if maketriangle
             end;
         end;
     end;
-flakem(1:800,:)=1;
-flakem(800:1004,1:2)=1;
-flakem(800:1004,1003:1004)=1;
+    flakem(1:800,:)=1;
+    flakem(800:1004,1:2)=1;
+    flakem(800:1004,1003:1004)=1;
 end;
 
 if makerevtriangle
@@ -257,19 +138,8 @@ if makerevtriangle
     flakem(msize-1,:)=0;
 end;
 
-% flakec=zeros(msize,nsize);
-
 if cat
-%     for i=3:msize-2
-%         if rem(i,numcat)==0
-%             flakem(3,i)=2;
-%             flakem(i,3)=2;
-%             flakem(msize-2,i)=2;
-%             flakem(i,nsize-2)=2;
-%         end;
-%     end;
     for i=1:catdist
-%         flakem(round(rand*(msize-7)+4),round(rand*(nsize-7)+4))=2;
         cat_xcoord=round(rand*(msize-5)+3);
         cat_ycoord=round(rand*(nsize-5)+3);
         if flakem(cat_xcoord,cat_ycoord)==1
@@ -291,12 +161,6 @@ opencross = [0 1 0;
              1 0 1; 
              0 1 0];
 circle = ones(3); circle(2, 2) = 0;
-
-
-% if cat
-% 	writeframe('cat', 0, 'dat', flakec)
-% end;
-% h = waitbar(0,'The earth will end in:');
 
 total = zeros(iter, 1);
 ASA = zeros(iter, 1);
@@ -378,10 +242,6 @@ for i=1:iter
     disp('End loop2')
     writeframe('flake', i, 'dat', flaket)
 
-    % 	if cat
-    % 	  writeframe('cat', i, 'dat', flakec)
-    % 	end;
-    
     subcat=2*size(find(flakem==2),1);
     conv(i+1)=sum(sum(flaket))-subcat;
     if conv(i+1)<=1
