1f00586b74a59df1e6abb6a312dbb64dca6b90c5
Carl Sandrock <carl.sandrock@rockconsulting.co.za>
2013-01-18 14:39:48 +0200

    Changed to display online graphics

--- flatflakec.py
+++ flatflakec.py
@@ -2,21 +2,21 @@
 
 import numpy as np
 import scipy.signal
-import render
+import matplotlib.pyplot as plt
 
 def conv2(in1, in2):
     return scipy.signal.convolve2d(in1, in2, 'same')
 
 writefile = True
-plot = False
+plot = True
 datatype = "uint8"
 
 np.random.seed(0)
 filestem = 'results/flake_py'
 
-maxiter = 1000 #4000
-msize = 504 # THIS IS Y
-nsize = 504 # THIS IS X
+maxiter = 700 #4000
+msize = 504
+nsize = msize
 
 normreac = 0.1
 
@@ -28,11 +28,7 @@ flakem[100:400, 250] = 0
 
 flaket = np.copy(flakem)
 
-if writefile:
-    render.writeframe(filestem, 0, 'dat', flakem)
-
-subcat = 2*(flakem == 2).sum()
-conv = [flakem.sum() - subcat]
+actives = [(flakem==1).sum()]
 
 # Stencils
 opencross = np.array([[0, 1, 0], 
@@ -50,9 +46,13 @@ xs = []
 countvec = range(1,6)
 
 if plot:
-    import matplotlib.pyplot as plt
-    plt.figure()
     plt.ion()
+    plt.subplot(2, 1, 1)
+    im = plt.imshow(flakem)
+    plt.subplot(2, 1, 2)
+    [activeline] = plt.plot(0, actives)
+    plt.xlim(0, maxiter)
+    plt.ylim(0, flakem.size)
 
 if writefile:
     import h5py
@@ -61,18 +61,14 @@ if writefile:
                                          (maxiter, msize, nsize),
                                          'uint8',
                                          compression='gzip',
-                                         chunks=(1,msize,nsize)
-                                         )
+                                         chunks=(1,msize,nsize))
     
 for i in xrange(maxiter):
-    print 'i =', i
-
     flakem = np.copy(flaket)
     
     opensum = conv2(flakem==0, opencross)
     counters = opensum * (flakem==1)
     counts,_ = np.histogram(counters, countvec)
-    print counts
 
     total.append(counts.sum())
     ASA.append(counts.sum() * countvec)
@@ -85,29 +81,30 @@ for i in xrange(maxiter):
     #bigcat_att = conv2(flakem, circle)
     # Figure out which graphite particles will react
     reactc = opensum
-    react = np.minimum(1, 1-(1-reactivity)**reactc)
-    willreact = np.random.rand(msize, nsize) < react
+    reactprob = np.minimum(1, 1-(1-reactivity)**reactc)
+    willreact = np.random.rand(msize, nsize) < reactprob
     flaket[willreact] = 0
     
-    if plot:
-        plt.imshow(flaket)
-        plt.draw()
-
     if writefile:
         framedata[i, :, :] = flaket
         framefile.flush()
-        #render.writeframe(filestem, i, 'dat', flaket)
-        
     
-    subcat = 2*(flakem==2).sum()
-    conv.append(flaket.sum()-subcat)
-    if conv[-1] <= 1:
+    actives.append((flaket==1).sum())
+    if actives[-1] <= 1:
         break
 
-    print 'Conversion:', (conv[0] - conv[-1])/conv[0]
+    if plot:
+        im.set_data(flaket)
+        activeline.set_data(np.arange(i+2), actives)
+        plt.draw()
+    else:
+        print 'i =', i
+        print counts
+        print 'Conversion:', (conv[0] - conv[-1])/conv[0]
+
+if writefile:
+    framefile.close()
 
 if plot:
     plt.show()
 
-if writefile:
-    framefile.close()
