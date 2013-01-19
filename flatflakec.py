2665bbcb5d3fabf66d7b16f46ce3e009a3dff4c7
Carl Sandrock <carl.sandrock@up.ac.za>
2013-01-19 10:20:13 +0200

    Catalyst activity added

--- flatflakec.py
+++ flatflakec.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Author: Heinrich Badenhorst 2012
+# Rewritten in Python by Carl Sandrock Jan 2013
+
 import numpy as np
 import scipy.signal
 import matplotlib.pyplot as plt
@@ -9,22 +12,32 @@ def conv2(in1, in2):
 
 writefile = True
 plot = True
+catalyst = True
+
 datatype = "uint8"
 
 np.random.seed(0)
-filestem = 'results/flake_py'
 
 maxiter = 700 #4000
 msize = 504
 nsize = msize
 
 normreac = 0.1
+catprob = 4.0/100 # Probability of catalyst 10000/(504*504) 
+catreac = 1
 
+# Initialise shape
 flakem = np.zeros([msize, nsize], datatype)
 flakem[2:-2, 2:-2] = 1
 flakem[50, 50:450] = 0
 flakem[450, 50:450] = 0
 flakem[100:400, 250] = 0
+
+if catalyst:
+    # Seed catalyst
+    # Using 9 for the catalyst allows us to use only one convolution to find the sum of graphite particles and the number of catalyst neighbours
+    flakem[(np.random.rand(msize, nsize) < catprob)
+           & (flakem==1)] = 9 
 
 flaket = np.copy(flakem)
 
@@ -37,6 +50,7 @@ opencross = np.array([[0, 1, 0],
 circle = np.ones([3, 3], datatype)
 circle[1, 1] = 0
 
+# Histories
 total = []
 ASA = []
 problog = []
@@ -71,20 +85,31 @@ for i in xrange(maxiter):
     counts,_ = np.histogram(counters, countvec)
 
     total.append(counts.sum())
-    ASA.append(counts.sum() * countvec)
+    ASA.append((counts * countvec[1:5]).sum())
     x = counts/total[-1]
     xs.append(x)
     reactivity=normreac
     problog.append(reactivity)
     allcounts.append(counts)
 
-    #bigcat_att = conv2(flakem, circle)
+    randoms = np.random.rand(msize, nsize)
     # Figure out which graphite particles will react
     reactc = opensum
     reactprob = np.minimum(1, 1-(1-reactivity)**reactc)
-    willreact = np.random.rand(msize, nsize) < reactprob
-    flaket[willreact] = 0
-    
+    willreact = randoms < reactprob
+    flaket[willreact & (flakem==1)] = 0
+
+    if catalyst:
+        closedsum = conv2(flakem, circle)
+        catopen = (1 <= closedsum) & (closedsum <= 7) & (flakem == 9)
+        for r, c in zip(*np.nonzero(catopen & (randoms < catreac))):
+            if 1 < r < nsize and 1 < c < msize:
+                whichone = np.random.randint(closedsum[r, c])
+                neighbourhood = flakem[r-1:r+2, c-1:c+2]
+                roffset, coffset = zip(*np.nonzero(neighbourhood==1))[whichone]
+                flaket[r, c] = 0
+                flaket[r+roffset-1, c+coffset-1] = 9
+        
     if writefile:
         framedata[i, :, :] = flaket
         framefile.flush()
@@ -100,7 +125,7 @@ for i in xrange(maxiter):
     else:
         print 'i =', i
         print counts
-        print 'Conversion:', (conv[0] - conv[-1])/conv[0]
+        print 'Conversion:', (actives[0] - actives[-1])/float(actives[0])
 
 if writefile:
     framefile.close()
