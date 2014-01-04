764f598336d8514437dc08695a32ebb825847184
Carl Sandrock <carl.sandrock@up.ac.za>
2014-01-04 11:23:11 +0200

    Switched to png library for rendering

--- render.py
+++ render.py
@@ -2,23 +2,16 @@
 
 import sys
 import numpy
-import matplotlib.pyplot as plt
 import h5py
+import png
 
 # It is better (faster) to use h5topng if it is available:
 # h5topng frames.h5 -o frame.png -m 0 -M 2 -c hot -x 1:600
 
-def plotframe(plt, frame, filename):
-    plt.imshow(frame)
-    plt.axis('off')
-    plt.savefig(filename)
-    plt.close()
-
-
 if __name__ == "__main__":
     filename = sys.argv[1]
     if len(sys.argv) == 2:
-        frameformat = "frame%4i.png"
+        frameformat = "frame%04i.png"
     else:
         frameformat = sys.argv[2]
         
@@ -26,5 +19,6 @@ if __name__ == "__main__":
     for i, frame in enumerate(framefile['frames']):
         filename = frameformat % i
         print filename
-        plotframe(plt, frame, filename)
+        png.from_array(frame*100, mode="L").save(filename)
+
         
